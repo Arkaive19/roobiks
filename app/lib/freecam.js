3f0f5caabcccf74b08944e47d413adfd905e5bf4
Arkaive19 <arkaive19@gmail.com>
2026-07-11 14:34:42 +0545

    Made front face culling, optimized some wrong maths, and also made a new wireframe drawing method

--- app/lib/freecam.js
+++ app/lib/freecam.js
@@ -1,8 +1,13 @@
 import visualizer3d from "./classes/visualizer.js";
-import { cubelets, roobiks } from "./cube-data.js";
+import { cubelets, roobiks, vs, fs } from "./cube-data.js";
 
 const renderer = new visualizer3d("canvas"); //new instance of my flagship A 3D RENDERER!
 /*renderer part is just to move the cube */
+const edges = fs.flatMap(([a, b, c]) => [
+  [a, b],
+  [b, c],
+  [c, a],
+]);
 document.addEventListener("keydown", (e) => {
   const key = e.key.toUpperCase();
   // console.log(key);
@@ -34,18 +39,22 @@ function frame() {
   renderer.updateCamera(); //updates camera
   renderer.clear(); //clears canvas
   renderer.startFrame(); //check visualizer.js for more info
-  for (const cubie of cubelets) {
-    //run for all cublets
-    const transform = roobiks.getCubieTransform(cubie);
-    renderer.drawMesh({
-      vertices: cubie.getVertices(),
-      edges: cubie.getEdges(),
-      faces: cubie.getFaces(),
-      rotation: transform.rotation,
-      pivot: transform.pivot,
-    }); //flagship method 💔💔💔💔
-  }
-  console.log(roobiks.steps().join(" "));
+  // for (const cubie of cubelets) {
+  //   //run for all cublets
+  //   const transform = roobiks.getCubieTransform(cubie);
+  //   renderer.drawMesh({
+  //     vertices: cubie.getVertices(),
+  //     edges: cubie.getEdges(),
+  //     faces: cubie.getFaces(),
+  //     rotation: transform.rotation,
+  //     pivot: transform.pivot,
+  //   }); //flagship method 💔💔💔💔
+  // }
+  renderer.drawWireframe({
+    vertices: vs,
+    edges: edges,
+    // translation: { x: 0, y: 0, z: 5 },
+  });
   renderer.endFrame();
   requestAnimationFrame(frame); //renderer makes the animation run at local display refresh rate!!!
 }
